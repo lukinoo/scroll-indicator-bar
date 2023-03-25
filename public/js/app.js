3a39d0079a6a36cf8bafd247f28d5e7b16134462
luka <luka.bliadze.2015@mail.ru>
2023-03-25 18:41:28 +0400

    ⚡IMPROVE: improving js code

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,55 @@
+// global variables
 const sections = document.querySelectorAll(".section");
-const indicators = document.querySelectorAll(".indicator");
 
-function handleScroll() {
+// function which creates indicator a tags
+const createPageIndicator = () => {
+  const pageIndocator = document.createElement("div");
+  pageIndocator.classList.add("page-indicator");
+
+  for (let i = 0; i < sections.length; i++) {
+    // section ids
+    const sectionId = sections[i].id;
+
+    // creating in
+    const indicator = document.createElement("a");
+    indicator.classList.add("indicator");
+    indicator.setAttribute("href", `#${sectionId}`);
+
+    // appending indicator in pageIndicator
+    pageIndocator.appendChild(indicator);
+
+    pageIndocator.childNodes[0].classList.add("active");
+  }
+
+  // appending page indicator in body
+  document.body.appendChild(pageIndocator);
+};
+
+createPageIndicator();
+
+const isSectionActive = (top, height) => {
+  if (window.pageYOffset >= top && window.pageYOffset < top + height)
+    return true;
+
+  return false;
+};
+
+const handleSectionScroll = () => {
+  const indicators = document.querySelectorAll(".indicator");
+
   sections.forEach((section, index) => {
     let top = section.offsetTop;
     let height = section.offsetHeight;
 
-    if (window.pageYOffset >= top && window.pageYOffset < top + height) {
+    if (isSectionActive(top, height)) {
       indicators[index].classList.add("active");
     } else {
       indicators[index].classList.remove("active");
     }
   });
-}
+};
 
+const handleScroll = () => handleSectionScroll();
+
+// events
 document.addEventListener("scroll", handleScroll);
